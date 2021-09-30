8fe239cdd7d5f2e5d4cb2dfd054bae0cbaf0756e
ahmedshrba <shrba9696@gmail.com>
2021-10-01 00:17:33 +0300

    Update task.js

--- task.js
+++ task.js
@@ -12,31 +12,39 @@ let numbers = [
 console.log("Original numbers list: ", numbers)
 
 for(let i = 0 ; i < numbers.length ; i++)
-{
+{  int swap=0;
     for(let j = 0 ; j < numbers.length - 1 ; j++)
     {
-        if(numbers[i] > numbers[j + 1])
+        if(numbers[j] > numbers[j + 1])
         {
             let temp = numbers[j];
             numbers[j] = numbers[j + 1]
             numbers[j + 1] = temp;
+           swap=1;
         }
     }
+   if(swap==0){
+    break;
+   }
 }
 
 console.log("Numbers list After sorting: ", numbers)
 
 for(let i = 0 ; i < numbers.length ; i++)
-{
+{  int swap=0;
     for(let j = 0 ; j < numbers.length - 1 ; j++)
     {
-        if(numbers[i] < numbers[j + 1])
+        if(numbers[j] < numbers[j + 1])
         {
             let temp = numbers[j];
             numbers[j] = numbers[j + 1]
             numbers[j + 1] = temp;
+            swap=1;
         }
     }
+  if(swap==0){
+    break;
+   }
 }
 
 console.log("Numbers list After Desc sorting: ", numbers)
